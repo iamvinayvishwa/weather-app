dfdbc90cd999e5982b6eda28f459ed6c4306da2c
Vinay Vishwakarma <vinayvishwakarma9919@gmail.com>
2024-12-04 12:46:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const API_KEY = '63c5cdcafabff56bd1ad421882a1c936'
-const form = document.querySelector("form")  //selector//
-const search = document.querySelector("#search")    //selector//
-const weather = document.querySelector("#weather")      //selector//
-
-
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 const getWeather = async(city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}`
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url);
     const data = await response.json()
     return showWeather(data)
 }
 
-
 const showWeather = (data) => {
-    console.log(data)
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
     weather.innerHTML = `
         <div>
-            <img src="https://openweathermap.org/img/wn/04n@2x.png" alt="">
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
         </div>
         <div>
             <h2>${data.main.temp} ℃</h2>
-            <h4>${data.weather[0].main}</h4>
+            <h4> ${data.weather[0].main} </h4>
         </div>
     `
 }
 
-
-
 form.addEventListener(
     "submit",
-    function(event){
-        getWeather(search.value)  //jo hum search karenge wo console me print ho jaayega//
-        event.preventDefault();  //stop form reload
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
     }
 )
-
